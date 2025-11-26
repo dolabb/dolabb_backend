0ceae34c57503518a3375fda8c7c045964ecc45a
Anas Pirzada <anaspirzada510@gmail.com>
2025-11-26 13:44:16 +0300

    Add URL routes for accepted offers endpoints

--- products/offer_urls.py
+++ products/offer_urls.py
@@ -3,7 +3,8 @@ Offer URLs
 """
 from django.urls import path
 from products.offer_views import (
-    create_offer, get_offers, accept_offer, reject_offer, counter_offer
+    create_offer, get_offers, accept_offer, reject_offer, counter_offer,
+    get_accepted_offers, get_accepted_offer_detail, upload_shipment_proof
 )
 
 urlpatterns = [
@@ -12,5 +13,8 @@ urlpatterns = [
     path('<str:offer_id>/accept/', accept_offer, name='accept_offer'),
     path('<str:offer_id>/reject/', reject_offer, name='reject_offer'),
     path('<str:offer_id>/counter/', counter_offer, name='counter_offer'),
+    path('accepted/', get_accepted_offers, name='get_accepted_offers'),
+    path('accepted/<str:offer_id>/', get_accepted_offer_detail, name='get_accepted_offer_detail'),
+    path('accepted/<str:offer_id>/upload-shipment-proof/', upload_shipment_proof, name='upload_shipment_proof'),
 ]
 
